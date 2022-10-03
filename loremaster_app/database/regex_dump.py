6ebcd83dcc6b5200f4e70f8752d5529964c88373
loftus6 <66481256+loftus6@users.noreply.github.com>
2022-10-03 18:10:07 -0400

    fancy password regex :trollface:

---
+++ loremaster_app/database/regex_dump.py
@@ -0,0 +1,11 @@
+import re
+
+email_regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+password_regex = re.compile(r'(?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[0-9]+)(?=.*\W+)(.{8,})+')
+#(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).*$
+
+def isValidEmail(email:str):
+    return re.fullmatch(email_regex, email)
+
+def isValidPassword(password:str):
+    return re.fullmatch(password_regex, password)
