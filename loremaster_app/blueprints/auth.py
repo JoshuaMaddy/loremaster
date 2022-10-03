68eaf4f7f86b5b6db3a560d698ac44e94b54bd24
JoshuaMaddy <110144926+JoshuaMaddy@users.noreply.github.com>
2022-10-03 18:11:08 -0400

    Basic user page

--- loremaster_app/blueprints/auth.py
+++ loremaster_app/blueprints/auth.py
@@ -1,4 +1,3 @@
-from unicodedata import name
 from cryptography.fernet import Fernet
 
 key = b'3G2g0L32hrr1zIiX4Bd369dWdnNzLK-WiSwh-d37LpM='
@@ -14,7 +13,7 @@ def decrypt(bytes_:bytes) -> str:
     return f.decrypt(bytes_).decode('utf-8')
 
 def check_password_hash(stored:bytes, given:str) -> bool:
-    """Determined if a password is correct based on the stored bytes."""
+    """Determined if a password is correct based on the stored bytes and given attempt."""
     return decrypt(stored) == given
 
 # Modified from: https://flask.palletsprojects.com/en/2.2.x/tutorial/views/
@@ -125,7 +124,7 @@ def load_logged_in_user():
     else:
         with Session.begin() as sqlsession:
             # Get user from DB
-            user = sqlsession.execute(select(User).where(User.id == user_id)).scalar()
+            user:User = sqlsession.execute(select(User).where(User.id == user_id)).scalar()
             
             # If found, expunge info (detach info) so it can be called out of sqlsession context.
             if user:
--- loremaster_app/blueprints/navigation.py
+++ loremaster_app/blueprints/navigation.py
@@ -2,9 +2,26 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for, send_file, jsonify
 )
 from flask import session as fsession
+from ..database.init_db import Session
+from ..database.table_declarations import *
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session as Ses
 
 bp = Blueprint('navi', __name__)
 
 @bp.route('/')
 def index():
     return render_template('homepage.html')
+
+@bp.route('/user/<int:user_id>')
+def user_page(user_id:int):
+    with Session.begin() as sqlsession:
+        sqlsession:Ses
+
+        user:User = sqlsession.execute(select(User).where(User.id == user_id)).scalar()
+
+        if user is None:
+            return redirect(url_for('navi.index'))
+        else:
+            return 'u found user good job :) ' + str(user_id)
