94e3789041663db7d0801256cc7721abacc844c8
JoshuaMaddy <110144926+JoshuaMaddy@users.noreply.github.com>
2022-10-04 12:16:05 -0400

    Fix neil's code :trollface:

--- loremaster_app/database/regex_dump.py
+++ loremaster_app/database/regex_dump.py
@@ -4,8 +4,8 @@ email_regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z
 password_regex = re.compile(r'(?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[0-9]+)(?=.*\W+)(.{8,})+')
 #(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).*$
 
-def isValidEmail(email:str):
+def isValidEmail(email:str) -> bool:
     return re.fullmatch(email_regex, email)
 
-def isValidPassword(password:str):
+def isValidPassword(password:str) -> bool:
     return re.fullmatch(password_regex, password)
