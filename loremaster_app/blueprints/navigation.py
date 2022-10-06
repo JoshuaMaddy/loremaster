bea6bf9b5e7c18b8c5265a88eb8223e69cd97393
JoshuaMaddy <110144926+JoshuaMaddy@users.noreply.github.com>
2022-10-06 13:45:14 -0400

    Updated character page, placeholder pages

--- loremaster_app/blueprints/navigation.py
+++ loremaster_app/blueprints/navigation.py
@@ -45,13 +45,37 @@ def character_page(character_id:int):
         if g.user:
             # Retrieve up to date user info
             user:User = sqlsession.execute(select(User).where(User.id == g.user.id)).scalar()
-            # If Character is owned/can be ediited by the viewer
+            # If Character is owned/can be edited by the viewer
             if character in user.editor_perms:
                 editor_perms = True
 
-        # If no character for id, fail silently
-        if character is None:
+        # If no character for id (or subclass of familiar), fail silently
+        if character is None or character.type == 'familiar':
             return redirect(url_for('navi.index'))
         else:
             # Pass character and editor perms to character page to be rendered
             return render_template('navigation/editables/character/character.html', editable=character, editor_perms=editor_perms)
+
+@bp.route('/familiar/<int:familiar_id>')
+def familiar_page(familiar_id:int):
+    return 'familiar page placeholder'
+
+@bp.route('/location/<int:location_id>')
+def location_page(location_id:int):
+    return 'location page placeholder'
+
+@bp.route('/inventory/<int:inventory_id>')
+def inventory_page(inventory_id:int):
+    return 'inventory page placeholder'
+
+@bp.route('/item/<int:item_id>')
+def item_page(item_id:int):
+    return 'item page placeholder'
+
+@bp.route('/image/<int:image_id>')
+def image_page(image_id:int):
+    return 'image page placeholder'
+
+@bp.route('/guild/<int:guild_id>')
+def guild_page(guild_id:int):
+    return 'guild page placeholder'
