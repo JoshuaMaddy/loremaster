fca47390738f5ae49cab1c71b242f742a12e772e
loftus6 <66481256+loftus6@users.noreply.github.com>
2022-10-12 19:34:12 -0400

    change to ASP.NET MVC

---
+++ LoreMaster/LoreMaster/LoreMaster/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoreMaster.MvcApplication" Language="C#" %>
---
+++ LoreMaster/LoreMaster/LoreMaster/Models/CharacterSheet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoreMaster.Models
+{
+    public class CharacterSheet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public int Level { get; set; }
+        public string Race { get; set; }
+        public string Series { get; set; }
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Wisdom { get; set; }
+        public int Intelligence { get; set; }
+        public int Charisma { get; set; }
+
+    }
+}
