3ffa7e83a59206edff6d493d5fb3ec627e6b6c4e
loftus6 <66481256+loftus6@users.noreply.github.com>
2022-10-12 21:26:10 -0400

    form progress

--- LoreMaster/LoreMaster/LoreMaster/Controllers/SheetsController.cs
+++ LoreMaster/LoreMaster/LoreMaster/Controllers/SheetsController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LoreMaster.Models;
 
 namespace LoreMaster.Controllers
 {
     public class SheetsController : Controller
     {
+        private ApplicationDbContext _dbContext;
         // GET: Sheets
+
+        public SheetsController ()
+        {
+            _dbContext = new ApplicationDbContext();    
+        }
         public ActionResult Index()
         {
-            return View();
+            var sheets = _dbContext.Sheets.ToList();
+            return View(sheets);
         }
     }
 }
